5f68413f50ba3e1e0e088403fafdb5ac8b54d8f5
ibrahim <ibrahinmesan@gmail.com>
2021-12-28 23:29:31 +0100

    keyboard avoid

--- App.js
+++ App.js
@@ -1,13 +1,12 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 import 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { Provider } from 'react-redux';
 import AuthProvider from './authProvider/AuthProvider';
 import MainStack from './navigation/MainStack';
-import HomeScreen from './screens/HomeScreen';
 import { sotre } from './store';
 
 
@@ -17,9 +16,11 @@ export default function App() {
       <AuthProvider>
         <SafeAreaProvider>
           <StatusBar style="auto" />
-          <NavigationContainer>
-              <MainStack />
-          </NavigationContainer>
+          <KeyboardAvoidingView style={{flex: 1}} behavior={Platform.OS === 'ios'? 'padding':'height'}>
+            <NavigationContainer>
+                <MainStack />
+            </NavigationContainer>
+          </KeyboardAvoidingView>
         </SafeAreaProvider>
       </AuthProvider>
     </Provider>
--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,19 +1,16 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { useSelector } from 'react-redux'
-import { selectDestination } from '../slices/navSlice'
+import React from 'react';
+import { Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import tw from 'twrnc';
 
 const RideOptionsCard = () => {
-    const destination = useSelector(selectDestination)
 
     return (
         <SafeAreaView>
-            <Text>{destination?.description}</Text>
+            <Text style={tw`text-center py-5 text-xl`}>Select a Ride</Text>
         </SafeAreaView>
     )
 }
 
 export default RideOptionsCard
 
-const styles = StyleSheet.create({})
