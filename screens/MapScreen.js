6837eacd2bae76f4ca4b0f57c44b45e671ad0f28
ibrahim <ibrahinmesan@gmail.com>
2022-01-04 21:54:28 +0100

    bottom sheet on map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,18 +1,29 @@
 import { useNavigation } from '@react-navigation/core';
 import { createStackNavigator } from '@react-navigation/stack';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useCallback, useMemo, useRef } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { FAB } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import tw from 'twrnc';
 import Map from '../components/Map';
 import NavigateCard from '../components/NavigateCard';
 import RideOptionsCard from '../components/RideOptionsCard';
+import BottomSheet from '@gorhom/bottom-sheet';
 
 const Stack = createStackNavigator();
 
 const MapScreen = () => {
     const navigation = useNavigation()
+
+    const bottomSheetRef = useRef(null);
+
+    // variables
+    const snapPoints = useMemo(() => ['10%', '100%'], []);
+
+    // callbacks
+    const handleSheetChanges = useCallback((index) => {
+        console.log('handleSheetChanges', index);
+    }, []);
 
     return (
         <View>          
@@ -30,6 +41,12 @@ const MapScreen = () => {
                 </View>
 
                 <View style={tw`h-1/2`}>
+                <BottomSheet
+                    ref={bottomSheetRef}
+                    index={1}
+                    snapPoints={snapPoints}
+                    onChange={handleSheetChanges}
+                >
                     <Stack.Navigator>
                         <Stack.Screen 
                             name='NavigateCard'
@@ -46,7 +63,27 @@ const MapScreen = () => {
                             }}
                         />
                     </Stack.Navigator>
+                </BottomSheet>
                 </View>
+
+                {/* <View style={tw`h-1/2`}>
+                    <Stack.Navigator>
+                        <Stack.Screen 
+                            name='NavigateCard'
+                            component={NavigateCard}
+                            options={{
+                                headerShown: false,
+                            }}
+                        />
+                        <Stack.Screen 
+                            name='RideOptionsCard'
+                            component={RideOptionsCard}
+                            options={{
+                                headerShown: false,
+                            }}
+                        />
+                    </Stack.Navigator>
+                </View> */}
             </SafeAreaView>
         </View>
     )
